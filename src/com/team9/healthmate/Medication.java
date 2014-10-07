65fb21695f1fd42abe959b1ee3db4c856999c4df
Gustavo Arce <gustavo.arce.719@my.csun.edu>
2014-10-06 23:35:36 -0700

    Simple UI, No functions
    Displaying SimpleList of elements and a Add New Medication button.

--- src/com/team9/healthmate/Medication.java
+++ src/com/team9/healthmate/Medication.java
@@ -1,16 +1,66 @@
 package com.team9.healthmate;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import com.team9.healthmate.R;
 
-public class Medication extends Activity {
+//Array of options --> array adapter --> listView
 
+public class Medication extends Activity {
+	public ListView medicationList;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_medication);
+		
+		String [] medicationString = {
+				"Medication A","Medication B","Medication C","Medication D","Medication E","Medication F"
+		};
+		
+		medicationList = (ListView) findViewById(R.id.listViewMedications);
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,medicationString);
+		medicationList.setAdapter(adapter);		
+	}	
+	
+	
+}
+
+class MedicationObject {
+	public static enum FrequencyLapse{
+		DAY, WEEK, MONTH
 	}
+	
+	String name;
+	FrequencyLapse lapse; 
+	int frequency; //e.g. take 'frequency' times a 'lapse'
+	int dosage; //in milligrams
+	
+	//Constructor
+	public MedicationObject(String name, FrequencyLapse lapse, int frequency, int dosage) {
+		this.name = name;
+		this.lapse = lapse;
+		this.frequency = frequency;
+		this.dosage = dosage;
+	}
+	
+	//Accessors
+	public String getName() { return this.name; }
+	public FrequencyLapse getFrequencyLapse() { return this.lapse; }
+	public int getFrequency() { return this.frequency; }
+	public int getDosage() { return this.dosage; }
+	
+	//Mutators
+	public void setName(String name) { this.name = name;  }	
+	public void setFrequencyLapse(FrequencyLapse lapse) { this.lapse = lapse; }
+	public void setFrequency(int frequency) { this.frequency = frequency; } 
+	public void setDosage(int dosage) { this.dosage = dosage; }
 }
