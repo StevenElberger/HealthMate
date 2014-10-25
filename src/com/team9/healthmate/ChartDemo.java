dffc55883f512d4186fb6429a9709765ad2cae1b
Steven Elberger <stevenelberger@gmail.com>
2014-10-25 11:01:19 -0700

    New stable version for graphs branch

--- src/com/team9/healthmate/ChartDemo.java
+++ src/com/team9/healthmate/ChartDemo.java
@@ -1,20 +1,18 @@
 package com.team9.healthmate;
 
-import lecho.lib.hellocharts.model.ColumnChartData;
-import lecho.lib.hellocharts.model.ColumnValue;
-import lecho.lib.hellocharts.model.LineChartData;
-import lecho.lib.hellocharts.view.ColumnChartView;
-import lecho.lib.hellocharts.view.LineChartView;
-
+import com.team9.healthmate.GraphManager.ColumnGraph;
 import com.team9.healthmate.GraphManager.GraphManager;
+import com.team9.healthmate.GraphManager.LineGraph;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 /**
  * Used to show graph generation
@@ -25,10 +23,9 @@ import android.view.View;
 public class ChartDemo extends ActionBarActivity {
 	View view;
 	Context context;
-	LineChartView lcv;
-	ColumnChartView ccv;
-	LineChartData lineData;
-	ColumnChartData columnData;
+	boolean genLineGraphs;
+	LineGraph[] lineGraph;
+	ColumnGraph[] columnGraph;
 	
 	/**
 	 * Sets up references for further use.
@@ -42,22 +39,35 @@ public class ChartDemo extends ActionBarActivity {
 		context = getApplicationContext();
 		view = findViewById(R.id.chart_layout);
 		
-		lcv = (LineChartView) findViewById(R.id.chart_top);
-		ccv = (ColumnChartView) findViewById(R.id.chart_bottom);
-		
-		ccv.setOnValueTouchListener(new ValueTouchListener());
+		createGraphs();
 	}
 	
-	private class ValueTouchListener implements ColumnChartView.ColumnChartOnValueTouchListener {
-
-		@Override
-		public void onValueTouched(int selectedLine, int selectedValue, ColumnValue value) {
-			GraphManager.updateLineData(lcv, context, value.getColor());
+	/**
+	 * Creates ColumnGraph and LineGraph
+	 * object arrays for later calls to the
+	 * GraphManager class.
+	 */
+	public void createGraphs() {
+		genLineGraphs = false;
+		
+		// We know the values for moods are 1 - 10 so we'll explicitly set the y-axis values
+		int[] yValues = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+		// Set up ColumnGraph objects for graph generation
+		String[] moods = {"General Wellness Levels", "Happiness Levels", "Motivation Levels", "Stress Levels", 
+				"Anger Levels", "Lethargy Levels", "Depression Levels"};
+		
+		// Other colors are possible
+		int[] colors = {Color.GREEN, Color.YELLOW, Color.CYAN, Color.MAGENTA, Color.RED, Color.LTGRAY, Color.DKGRAY};
+		
+		columnGraph = new ColumnGraph[moods.length];
+		for (int i = 0; i < columnGraph.length; i++) {
+			columnGraph[i] = new ColumnGraph("Days", moods[i], "testdata", null, yValues, false, false, colors[i]);
 		}
-
-		@Override
-		public void onNothingTouched() {
-			
+		
+		lineGraph = new LineGraph[moods.length];
+		for (int i = 0; i < lineGraph.length; i++) {
+			lineGraph[i] = new LineGraph("Days", moods[i], "testdata", null, yValues, false, true, colors[i], 1, 3);
 		}
 	}
 	
@@ -78,13 +88,65 @@ public class ChartDemo extends ActionBarActivity {
 	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		// Necessary to wipe the view where the graph
+		// will be displayed
+		((RelativeLayout) view).removeAllViews();
+
 		// Generate a new ColumnGraph object with desired
 		// properties depending on which option was selected
 		// and then generate a new Column Graph to display
-	    if (item.getItemId() == R.id.JustOk) {
-	    	GraphManager.setUpInitialLineChart(context, lcv);
-    		GraphManager.setUpMoodColumnGraph(context, ccv);
+	    switch (item.getItemId()) {
+	        case R.id.JustOk:
+	        	if (genLineGraphs) {
+	        		GraphManager.generateMoodLineGraph(context, view, lineGraph[0], "Just Ok");
+	        	} else {
+	        		GraphManager.generateMoodColumnGraph(context, view, columnGraph[0], "Just Ok");
+	        	}
+	            return true;
+	        case R.id.Happy:
+	        	if (genLineGraphs) {
+	        		GraphManager.generateMoodLineGraph(context, view, lineGraph[1], "Happy");
+	        	} else {
+	        		GraphManager.generateMoodColumnGraph(context, view, columnGraph[1], "Happy");
+	        	}
+	            return true;
+	        case R.id.Motivated:
+	        	if (genLineGraphs) {
+	        		GraphManager.generateMoodLineGraph(context, view, lineGraph[2], "Motivated");
+	        	} else {
+	        		GraphManager.generateMoodColumnGraph(context, view, columnGraph[2], "Motivated");
+	        	}
+	        	return true;
+	        case R.id.Stressed:
+	        	if (genLineGraphs) {
+	        		GraphManager.generateMoodLineGraph(context, view, lineGraph[3], "Stressed");
+	        	} else {
+	        		GraphManager.generateMoodColumnGraph(context, view, columnGraph[3], "Stressed");
+	        	}
+	        	return true;
+	        case R.id.Angry:
+	        	if (genLineGraphs) {
+	        		GraphManager.generateMoodLineGraph(context, view, lineGraph[4], "Angry");
+	        	} else {
+	        		GraphManager.generateMoodColumnGraph(context, view, columnGraph[4], "Angry");
+	        	}
+	        	return true;
+	        case R.id.Tired:
+	        	if (genLineGraphs) {
+	        		GraphManager.generateMoodLineGraph(context, view, lineGraph[5], "Tired");
+	        	} else {
+	        		GraphManager.generateMoodColumnGraph(context, view, columnGraph[5], "Tired");
+	        	}
+	        	return true;
+	        case R.id.Depressed:
+	        	if (genLineGraphs) {
+	        		GraphManager.generateMoodLineGraph(context, view, lineGraph[6], "Depressed");
+	        	} else {
+	        		GraphManager.generateMoodColumnGraph(context, view, columnGraph[6], "Depressed");
+	        	}
+	        	return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
 	    }
-	    return true;
 	}
 }
