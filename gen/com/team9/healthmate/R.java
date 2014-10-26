545d6c0685d05c9edc2e7bf3aeaaa877cd5afab3
G4mouse <avetikyan.davit@gmail.com>
2014-10-26 11:11:07 -0700

    change made on moods

--- gen/com/team9/healthmate/R.java
+++ gen/com/team9/healthmate/R.java
@@ -80,12 +80,11 @@ public final class R {
         public static final int activity_health_location=0x7f030001;
         public static final int activity_login=0x7f030002;
         public static final int activity_medication=0x7f030003;
-        public static final int activity_menu=0x7f030004;
-        public static final int activity_moods=0x7f030005;
-        public static final int activity_note=0x7f030006;
-        public static final int activity_registration=0x7f030007;
-        public static final int activity_step_counter=0x7f030008;
-        public static final int sex_spinner_textview=0x7f030009;
+        public static final int activity_moods=0x7f030004;
+        public static final int activity_note=0x7f030005;
+        public static final int activity_registration=0x7f030006;
+        public static final int activity_step_counter=0x7f030007;
+        public static final int sex_spinner_textview=0x7f030008;
     }
     public static final class menu {
         public static final int registration=0x7f090000;
