f09a55743f613b9772e6e4effcb2952d9938e982
Steven Elberger <stevenelberger@gmail.com>
2014-11-13 22:57:12 -0800

    Add profile button to main menu, create UserProfile.java

---
+++ src/com/team9/healthmate/UserProfile.java
@@ -0,0 +1,16 @@
+package com.team9.healthmate;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class UserProfile extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user_profile);
+	}
+}
