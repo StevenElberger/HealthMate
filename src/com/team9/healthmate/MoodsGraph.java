fcc921858bf6100bcbd34696e77289e093a8e401
G4mouse <avetikyan.davit@gmail.com>
2014-11-16 13:10:27 -0800

    Changes on MoodsGraph will be commited

--- src/com/team9/healthmate/MoodsGraph.java
+++ src/com/team9/healthmate/MoodsGraph.java
@@ -34,8 +34,7 @@ import android.widget.RadioButton;;
  *	The data is received from local stored 
  *	android file.
  */
-public class MoodsGraph extends Fragment
-						{
+public class MoodsGraph extends Fragment{
 	
 	View view;
 	Context context;
@@ -47,9 +46,7 @@ public class MoodsGraph extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
-        View rootView = inflater.inflate(R.layout.fragment_moods_graph, container, false);
-        
-       
+        View rootView = inflater.inflate(R.layout.fragment_moods_graph, container, false);    
          
         return rootView;
 }	
