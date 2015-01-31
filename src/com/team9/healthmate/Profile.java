10a927c7014d6a691148f94bc9351c76e31941c9
Steven Elberger <stevenelberger@gmail.com>
2015-01-31 13:27:05 -0800

    Profiles are up and back to normal

--- src/com/team9/healthmate/Profile.java
+++ src/com/team9/healthmate/Profile.java
@@ -1,6 +1,7 @@
 package com.team9.healthmate;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -17,9 +18,14 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -27,6 +33,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -42,7 +49,7 @@ import android.widget.TextView;
  * @author Steve
  */
 public class Profile extends Activity {
-	String sFirstName, sLastName, sUserName, sGender, sAge, sImagePath;
+	String sFirstName, sLastName, sUserName, sPassword, sGender, sAge, sImagePath;
 	boolean notPressedYet = true;
 	private static final String IMAGE_PATH = "Image_Path";
 	private static final String FIRST_NAME = "First_Name";
@@ -90,33 +97,68 @@ public class Profile extends Activity {
 		FragmentManager fm = getFragmentManager();
 		
 		if (item.getItemId() == R.id.action_edit_profile) {
+			// Going from the DisplayFragment to the EditFragment
+			DisplayFragment df = (DisplayFragment) fm.findFragmentById(R.id.profile_container);
 			EditFragment ef = new EditFragment();
+			
 			// Bundle the info and send it to the new fragment
 			Bundle profileInformation = new Bundle();
-			profileInformation.putString(IMAGE_PATH, sImagePath);
-			profileInformation.putString(FIRST_NAME, sFirstName);
-			profileInformation.putString(LAST_NAME, sLastName);
-			profileInformation.putString(USERNAME, sUserName);
-			profileInformation.putString(GENDER, sGender);
-			profileInformation.putString(AGE, sAge);
+			profileInformation.putString(IMAGE_PATH, df.sImagePath);
+			profileInformation.putString(FIRST_NAME, df.sFirstName);
+			profileInformation.putString(LAST_NAME, df.sLastName);
+			profileInformation.putString(USERNAME, df.sUserName);
+			profileInformation.putString(GENDER, df.sGender);
+			profileInformation.putString(AGE, df.sAge);
 			ef.setArguments(profileInformation);
-			fm.beginTransaction().replace(R.id.profile_container, ef).commit(); // really should just be hidden instead of replaced
+			
+			// Really should just be hidden instead of replaced, but #YOLO
+			fm.beginTransaction().replace(R.id.profile_container, ef).commit();
 			notPressedYet = false;
+			invalidateOptionsMenu();
+			
 		} else if (item.getItemId() == R.id.action_save_profile) {
-			// Show the DisplayFragment with new information
-			//getFragmentManager().beginTransaction().replace(R.id.profile_container, new DisplayFragment()).commit();
+			// Going from the EditFragment to the DisplayFragment
+			EditFragment ef = (EditFragment) fm.findFragmentById(R.id.profile_container);
+			DisplayFragment df = new DisplayFragment();
+			
+			// Grab from the DatePicker
+			int day = ef.age.getDayOfMonth();
+			int month = ef.age.getMonth();
+			int year = ef.age.getYear();
+			Calendar c = Calendar.getInstance();
+			c.set(year,  month, day);
+			Date userAge = c.getTime();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+			String dateString = sdf.format(userAge);
+			String sUserAge = dateString;
+			
+			// Check to make sure user entered his/her password
+			sPassword = ef.ePassword.getText().toString();
+			if (sPassword.equals("")) {
+				TextView warningLabel = (TextView) findViewById(R.id.edit_profile_fragment_no_password_label);
+				warningLabel.setTextColor(Color.RED);
+				warningLabel.setVisibility(0);
+			} else {
+				// Write information to user account file
+				// and bring user back to the display fragment
+				writeProfileInformation();
+				
+				// Show the DisplayFragment with new information
+				Bundle profileInformation = new Bundle();
+				profileInformation.putString(IMAGE_PATH, ef.sImagePath);
+				profileInformation.putString(FIRST_NAME, ef.eFirstName.getText().toString());
+				profileInformation.putString(LAST_NAME, ef.eLastName.getText().toString());
+				profileInformation.putString(USERNAME, ef.eUserName.getText().toString());
+				profileInformation.putString(GENDER, ef.gender.getSelectedItem().toString());
+				profileInformation.putString(AGE, sUserAge);
+				df.setArguments(profileInformation);
+				
+				fm.beginTransaction().replace(R.id.profile_container, df).commit();
+				notPressedYet = true;
+				invalidateOptionsMenu();
+			}
 		}
 		return super.onOptionsItemSelected(item);
-	}
-	
-	@Override
-	public void onSaveInstanceState(Bundle savedInstanceState) {
-		super.onSaveInstanceState(savedInstanceState);
-		savedInstanceState.putString(FIRST_NAME, sFirstName);
-		savedInstanceState.putString(LAST_NAME, sLastName);
-		savedInstanceState.putString(USERNAME, sUserName);
-		savedInstanceState.putString(GENDER, sGender);
-		savedInstanceState.putString(AGE, sAge);
 	}
 	
 	/**
@@ -166,7 +208,29 @@ public class Profile extends Activity {
 		}
 	}
 	
-
+	/**
+	 * Takes account information from forms being displayed
+	 * and writes them to the user account file.
+	 */
+	public void writeProfileInformation() {
+		try {
+			Context context = getApplicationContext();
+			Map<String, String> dataSet = new HashMap<String, String>();
+			dataSet.put("username", sUserName);
+			dataSet.put("first_name", sFirstName);
+			dataSet.put("last_name", sLastName);
+			dataSet.put("sex", sGender);
+			dataSet.put("age", sAge);
+			dataSet.put("password", sPassword);
+			if (sImagePath == null) {
+				sImagePath = "";
+			}
+			dataSet.put("picture", sImagePath);
+			DataStorageManager.writeJSONObject(context, "account", dataSet, true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	/**
 	 * This fragment is responsible for displaying the profile
 	 * data. 
@@ -221,6 +285,7 @@ public class Profile extends Activity {
 	 */
 	public static class EditFragment extends Fragment {
 		ImageView profileImage;
+		Button uploadButton;
 		Spinner gender;
 		DatePicker age;
 		String sFirstName, sLastName, sUserName, sGender, sAge, sImagePath;
@@ -232,11 +297,27 @@ public class Profile extends Activity {
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_edit_profile, container, false);
 			
+			// set up the upload button
+			uploadButton = (Button) rootView.findViewById(R.id.edit_profile_fragment_upload_profile_pic);
+			
+			uploadButton.setOnClickListener(new Button.OnClickListener() {
+				
+				// When clicked, launch the photo gallery
+				// so the user may select a profile picture
+				@Override
+				public void onClick(View v) {
+					Intent intent = new Intent(Intent.ACTION_PICK,
+							android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+					startActivityForResult(intent, 0);
+				}
+			});
+			
 			// Wire the widgets
 			profileImage = (ImageView) rootView.findViewById(R.id.edit_profile_fragment_edit_profile_pic);
 			eFirstName = (EditText) rootView.findViewById(R.id.edit_profile_fragment_f_name);
 			eLastName = (EditText) rootView.findViewById(R.id.edit_profile_fragment_l_name);
 			eUserName = (EditText) rootView.findViewById(R.id.edit_profile_fragment_u_name);
+			ePassword = (EditText) rootView.findViewById(R.id.edit_profile_fragment_password);
 			gender = (Spinner) rootView.findViewById(R.id.edit_profile_fragment_gender);
 			age = (DatePicker) rootView.findViewById(R.id.edit_profile_fragment_bday);
 			
@@ -287,6 +368,54 @@ public class Profile extends Activity {
 			}
 			
 			return rootView;
+		}
+		
+		/**
+		 * Returns the path to an image file selected
+		 * from the photo gallery. This is used to save
+		 * the image path of the user's profile picture
+		 * to the user's account file.
+		 * @param context		Application context
+		 * @param contentUri	The URI being returned with the image data
+		 * @return				A string of the image's direct file path
+		 */
+		public String getRealPathFromURI(Context context, Uri contentUri) {
+			Cursor cursor = null;
+			try { 
+				String[] proj = { MediaStore.Images.Media.DATA };
+			    cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+			    int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+			    cursor.moveToFirst();
+			    return cursor.getString(column_index);
+			} finally {
+				if (cursor != null) {
+					cursor.close();
+			    }
+			}
+		}
+		
+		/**
+		 * Grabs the data returned from the photo gallery.
+		 * This will be used to load the profile picture in the
+		 * user's profile.
+		 */
+		@Override
+		public void onActivityResult(int requestCode, int resultCode, Intent data) {
+			 super.onActivityResult(requestCode, resultCode, data);
+			 
+			 // grab the image's path to save it
+			 // to the account file and load the image
+			 if (resultCode == RESULT_OK) {
+				 Uri targetUri = data.getData();
+				 sImagePath = getRealPathFromURI(getActivity().getApplicationContext(), targetUri);
+				 Bitmap bitmap;
+				 try {
+					 bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(targetUri));
+					 profileImage.setImageBitmap(bitmap);
+				 } catch (FileNotFoundException e) {
+					 e.printStackTrace();
+				 }
+			 }
 		}
 	}
 }
