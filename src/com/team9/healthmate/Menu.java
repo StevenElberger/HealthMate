b958cb502fd8126ad1836b8ec246442ee0611fc5
Gustavo Arce <gustavo.arce.719@my.csun.edu>
2015-02-22 16:14:34 -0800

    Main Menu Updated
    Main menu is now displayed as a dashboard menu

--- src/com/team9/healthmate/Menu.java
+++ src/com/team9/healthmate/Menu.java
@@ -19,6 +19,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -28,24 +29,43 @@ import com.team9.healthmate.Medications.Medication;
 import com.team9.healthmate.Notes.ListOfNotes;
 
 public class Menu extends Activity {
-	public ListView menu;
+	GridView menu_grid;
 	public Intent intent;
 	Drawable pic;
 	
+	String[] menuString = {
+			"Moods","Medication","Steps","Health Locations","Notes", "Appointments", "Contact My Doctor"
+	}; // "Graphs", "Animated Graphs", "More Graphs",
+	
+	int[] imageId = {
+		      R.drawable.ic_heart,
+		      R.drawable.ic_heart,
+		      R.drawable.ic_heart,
+		      R.drawable.ic_heart,
+		      R.drawable.ic_heart,
+		      R.drawable.ic_heart,
+		      R.drawable.ic_heart
+		  };
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_menu);
-		
-		String [] menuString = {
-				"Moods","Medication","Steps","Health Locations","Notes", "Appointments", "Contact My Doctor"
-		}; // "Graphs", "Animated Graphs", "More Graphs",
-		menu = (ListView) findViewById(R.id.menu);
-		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,menuString);
-		menu.setAdapter(adapter);
-		/*This is a listener to find the users option selected */
-		menu.setOnItemClickListener(new OnItemClickListener() {
+	  protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_menu);
+	    CustomGridView adapter = new CustomGridView(Menu.this, menuString, imageId);
+	    menu_grid = (GridView)findViewById(R.id.menu);
+	    menu_grid.setAdapter(adapter);
+	    
+	    /*
+	    menu_grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(Menu.this, "You Clicked at " +menuString[+ position], Toast.LENGTH_SHORT).show();
+            }
+        });
+        */
+	    
+	    menu_grid.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -54,44 +74,8 @@ public class Menu extends Activity {
 
 			}
 		});
-	}
-	
-	public void changeActivity(int pos)	{
-		intent = new Intent(this, getMenuItem(pos));
-		startActivity(intent);
-	}
-	
-	/* switch statement to find the correct option to go to */
-	public Class<?> getMenuItem(int pos)	{
-		switch(pos)	{
-			case 0: return Moods.class; 
-			case 1: return Medication.class;
-			case 2: return StepCounter.class;
-			case 3:	return HealthLocation.class;
-			case 4:	return ListOfNotes.class;
-			case 5: return AppointmentsList.class;
-//			case 6: return ChartDemo.class;
-//			case 7: return AnimatedChartDemo.class;
-//			case 8: return PreviewChartDemo.class;
-			case 6: return ContactMyDoctor.class;
-		}
-		return null;
-	}
-	
-	/**
-	 * Set up the user profile button on the action bar.
-	 */
-	@Override
-	public boolean onCreateOptionsMenu(android.view.Menu menu) {
-	    // Inflate the menu items for use in the action bar
-		getMenuInflater().inflate(R.menu.main_menu, menu);
-		loadProfileInformation();
-		if (pic != null) {
-			menu.getItem(0).setIcon(pic);
-		}
-		return true;
-	}
-	
+	  }
+
 	/**
 	 * Takes account information from local storage and
 	 * displays them on the appropriate forms in the profile
@@ -125,6 +109,42 @@ public class Menu extends Activity {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public void changeActivity(int pos)	{
+		intent = new Intent(this, getMenuItem(pos));
+		startActivity(intent);
+	}
+
+	/* switch statement to find the correct option to go to */
+	public Class<?> getMenuItem(int pos)	{
+		switch(pos)	{
+			case 0: return Moods.class; 
+			case 1: return Medication.class;
+			case 2: return StepCounter.class;
+			case 3:	return HealthLocation.class;
+			case 4:	return ListOfNotes.class;
+			case 5: return AppointmentsList.class;
+//			case 6: return ChartDemo.class;
+//			case 7: return AnimatedChartDemo.class;
+//			case 8: return PreviewChartDemo.class;
+			case 6: return ContactMyDoctor.class;
+		}
+		return null;
+	}
+
+	/**
+	 * Set up the user profile button on the action bar.
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(android.view.Menu menu) {
+	    // Inflate the menu items for use in the action bar
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		loadProfileInformation();
+		if (pic != null) {
+			menu.getItem(0).setIcon(pic);
+		}
+		return true;
 	}
 	
 	/**
